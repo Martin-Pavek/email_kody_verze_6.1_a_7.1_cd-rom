f159c0268518337d91928462dd2763a54ccf1a82
Martin-Pavek <146820572+Martin-Pavek@users.noreply.github.com>
2023-10-03 11:42:50 +0200

    Add files via upload

---
+++ clone.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+git clone https://github.com/Martin-Pavek/email_kody_verze_6.1_a_7.1_cd-rom
+cd email_kody_verze_6.1_a_7.1_cd-rom/
+rar t email_kody_verze_6.1_a_7.1_cd-rom.rar
+sleep 5
+rar x email_kody_verze_6.1_a_7.1_cd-rom.rar
+
